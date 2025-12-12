2ca276604fc436fa23cdf6ddb43795f54d182809
12LuA <71656192+12LuA@users.noreply.github.com>
2025-12-12 18:05:20 +0100

    feat: update layout and add documentation layout for python-sdk

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,11 +1,30 @@
 import { source } from '@/lib/source'
-import { DocsLayout } from 'fumadocs-ui/layouts/docs'
+import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/docs'
 import { baseOptions } from '@/lib/layout.shared'
+import { Book } from 'lucide-react'
+import { BracesIcon, CodeIcon, ImagePlusIcon, WorkflowIcon } from 'lucide-react'
 
-export default function Layout({ children }: LayoutProps<'/docs'>) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
-      {children}
-    </DocsLayout>
-  )
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions(),
+  tree: source.pageTree,
+  sidebar: {
+    tabs: [
+      {
+        title: 'API Docs',
+        
+        url: '/docs',
+        icon: <Book className="h-4 w-4" />,
+      },
+      {
+        title: 'python-sdk',
+        
+        url: '/docs/python-sdk',
+        icon: <CodeIcon className="text-green-300 h-4 w-4" />,
+      },
+    ],
+  },
+}
+
+export default function Layout({ children }: LayoutProps<'/'>) {
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>
 }
